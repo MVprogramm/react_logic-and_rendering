34c378dc6d2fc96968b45893952c887265cf03a3
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-06-02 08:39:34 +0300

    add project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './index.scss';
+
+import Mailbox from './Mailbox.jsx';
+
+const App = () => {
+  return <Mailbox unreadMessages={[]} />
+}
+
+export default App;
